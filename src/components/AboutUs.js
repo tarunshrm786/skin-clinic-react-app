607849f233201eb9e6cf879aca1d71b53d1976c5
Tarun Sharma <tarunshrm768@gmail.com>
2024-10-09 08:24:49 +0530

    commit

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,6 +1,5 @@
 // import React from 'react';
 // import '../css/AboutUs.css'; // Import your custom CSS for styling
-// //import aboutImage from '../images/about-image.jpg'; // Replace with your image path
 
 // const AboutUs = () => {
 //   return (
@@ -16,10 +15,13 @@
 //           <h1>About Us</h1>
 //           <h2>DR. GEETA MEHRA FAZALBHOY</h2>
 //           <p>
-//           Dr. Geeta Mehra Fazalbhoy, MD & Founder of Skin & You Clinic, Mumbai, is a globally recognized expert in dermatology and aesthetic medicine. With advanced international training and certifications, she specializes in cutting-edge, pain-free treatments for skin tightening, pigmentation, and body contouring. Her clinic is ranked among the top in India, attracting patients worldwide.
-
-
+//             Dr. Geeta Mehra Fazalbhoy, MD & Founder of Skin & You Clinic, Mumbai, is a globally recognized expert in dermatology and aesthetic medicine. With advanced international training and certifications, she specializes in cutting-edge, pain-free treatments for skin tightening, pigmentation, and body contouring. Her clinic is ranked among the top in India, attracting patients worldwide.
 //           </p>
+//           {/* Buttons */}
+//           <div className="button-container">
+//             <button className="know-more-button">Know More</button>
+//             <button className="call-now-button">Instagram</button>
+//           </div>
 //         </div>
 //       </div>
 //     </section>
