32f90c81a636ebb8b56282344ed927e6fee15773
Tarun Sharma <tarunshrm768@gmail.com>
2024-10-09 06:38:01 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,3 @@
-import React from 'react';
-import '../css/Header.css';
-
-const Header = () => {
-  return (
-    <header className="header">
-      <nav className="nav-menu">
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#treatments">Treatments</a></li>
-          <li><a href="#concern">Treatment by Concern</a></li>
-          <li><a href="#media">Media</a></li>
-          <li><a href="#aboutus">About Us</a></li>
-          <li><a href="#contactus">Contact Us</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
-
-export default Header;
-
 // import React from 'react';
 // import '../css/Header.css';
 
@@ -29,15 +7,8 @@ export default Header;
 //       <nav className="nav-menu">
 //         <ul>
 //           <li><a href="#home">Home</a></li>
-//           <li className="dropdown">
-//             <a href="#treatments" className="dropdown-link">Treatments</a>
-//             <ul className="dropdown-menu">
-//               <li><a href="#concern">Treatment by Concern</a></li>
-//               <li><a href="#skin-care">Skin Care</a></li>
-//               <li><a href="#hair-care">Hair Care</a></li>
-//               <li><a href="#anti-aging">Anti-aging</a></li>
-//             </ul>
-//           </li>
+//           <li><a href="#treatments">Treatments</a></li>
+//           <li><a href="#concern">Treatment by Concern</a></li>
 //           <li><a href="#media">Media</a></li>
 //           <li><a href="#aboutus">About Us</a></li>
 //           <li><a href="#contactus">Contact Us</a></li>
@@ -48,3 +19,56 @@ export default Header;
 // };
 
 // export default Header;
+
+import React from 'react';
+import '../css/Header.css';
+import { FaSearch } from 'react-icons/fa'; // Import search icon
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-container">
+        {/* Left: Logo */}
+        <div className="logo">
+          <img src="/logo-2.png" alt="Logo" />
+        </div>
+
+        {/* Middle: Search Bar */}
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button className="search-btn">
+            <FaSearch />
+          </button>
+        </div>
+
+        {/* Right: Navigation Menu */}
+        <nav className="nav-menu">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#treatments">Treatments</a></li>
+            <li className="dropdown">
+              <a href="#concern">Treatment by Concern</a>
+              {/* Dropdown for Treatment by Concern */}
+              <ul className="dropdown-menu">
+                <li><a href="#concern1">Concern 1</a></li>
+                <li><a href="#concern2">Concern 2</a></li>
+              </ul>
+            </li>
+            <li><a href="#media">Media</a></li>
+            <li className="dropdown">
+              <a href="#aboutus">About Us</a>
+              {/* Dropdown for About Us */}
+              <ul className="dropdown-menu">
+                <li><a href="#history">History</a></li>
+                <li><a href="#team">Our Team</a></li>
+              </ul>
+            </li>
+            <li><a href="#contactus">Contact Us</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
